262b0a278c16e7ec096d52ec314f2a7dfed7aad7
Vizeu <leovizeu@hotmail.com>
2024-01-29 11:38:45 -0300

    finished the code

--- game.js
+++ game.js
@@ -1,23 +1,40 @@
-const choices = ["rock", "paper", "scissors"]
 
-function game () {
-    //Play 5 rounds
-    //console game
-    playRound()
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper beats rock";
+        } else {
+            return "You win! Rock beats scissors";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats rock";
+        } else {
+            return "You lose! Scissors beats paper";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock beats scissors";
+        } else {
+            return "You win! Scissors beats paper";
+        }
+    }
 }
 
-function playRound () {
-    const playerSelection = playerChoice()
-    const computerSelection = computerChoice()
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * 3);
+    return choices[randomChoice];
 }
 
-function playerChoice () {
-    //get input from player
-    let input = prompt("Chose between Rock, Paper or Scissors")
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, paper, or scissors?");
+    playerChoice = playerChoice.toLowerCase();
+    return playerChoice;
 }
 
-function computerChoice () {
-    return choices [Math.floor(Math.random() * choices.length)]
-}
-
-game()
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
